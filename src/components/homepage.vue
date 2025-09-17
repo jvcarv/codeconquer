b499797e5c586c05802b695776b01540f55b8585
joaoantoniazzi <joao.antoniazzi.dev@gmail.com>
2025-09-17 20:38:46 -0300

    Adds style to complaint cards

--- src/components/homepage.vue
+++ src/components/homepage.vue
@@ -10,7 +10,6 @@ const btnStrings = {
 
 let complaints = null;
 const showComplaints = ref(false);
-const btnStr = ref(btnStrings[showComplaints ? 'true' : 'false'])
 
 function toggleComplaintsView() {
   showComplaints.value = !showComplaints.value
@@ -23,11 +22,11 @@ function toggleComplaintsView() {
   <v-btn v-show="!showComplaints" variant="plain" class="btn--view_existing" @click="toggleComplaintsView">Ver Denúncias Existentes</v-btn>
   <v-btn v-show="showComplaints" variant="plain" class="btn--view_existing" @click="toggleComplaintsView">Esconder Denúncias</v-btn>
 
-  <div class="complaints__box">
+  <div class="complaints__box" v-show="showComplaints">
     <div class="complaints__item" v-for="item of complaints">
-      <span class="complaints__category"></span>
-      <span class="complaints__description"></span>
-      <span class="complaints__city"></span>
+      <span class="complaints__category">{{ item.category }}</span>
+      <span class="complaints__description">{{ item.description }}</span>
+      <span class="complaints__city">{{ item.city }}</span>
     </div>
   </div>
 
@@ -103,5 +102,44 @@ button.v-btn.btn--main {
   text-transform: capitalize;
 
   letter-spacing: normal;
+}
+
+.complaints__box {
+  display: flex;
+  padding: 0 20px;
+  flex-direction: column;
+  gap: 8px;
+
+  margin-bottom: 32px;
+}
+
+.complaints__item {
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  padding: 12px 16px;
+  border-radius: 12px;
+
+  border: solid 1px #e5e7eb;
+}
+
+.complaints__category {
+  color: #344256;
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 20px;
+}
+
+.complaints__description {
+  color: #9aa4b1;
+  font-size: 14px;
+  text-align: left;
+}
+
+.complaints__city {
+  color: #666d75;
+  font-size: 14px;
+  font-weight: 700;
+  text-align: left;
 }
 </style>
